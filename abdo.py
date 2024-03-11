0e8d1716ba0f0062457f7a3a6b8f905364d288af
kaboos11 <161256961+kaboos11@users.noreply.github.com>
2024-03-11 15:22:45 +0200

    Delete abdo.py

--- abdo.py
+++
@@ -1,8 +0,0 @@
-from telethon.sync import TelegramClient
-from telethon.sessions import StringSession
-import os
-APP_ID = os.environ.get("APP_ID", "21511060")
-APP_HASH = os.environ.get("APP_HASH", "b2a2178e131fc47896c65d2d5e0c1faa")
-SESSION = os.environ.get("SESSION")
-elhzeyba = TelegramClient(StringSession(SESSION), APP_ID, APP_HASH)
-elhzeyba.start()
